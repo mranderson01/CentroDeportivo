234d28d8754bbc341aeb19507ee1cb4e760551f4
Rodrigo Sucapuca Alaca (ext) <rodrigo.sucapuca.external@eviden.com>
2024-04-16 09:30:08 +0100

    insertando proyecto centro deportivo java backend

---
+++ entrenamiento/src/test/java/es/centro/entrenamiento/EntrenamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.centro.entrenamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
